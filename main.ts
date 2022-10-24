8624cfdc72fb69eb2ddd5a5ebf2aa79940bfcdc4
Lucas Sutherland <sutherlandlucas1@gmail.com>
2022-10-24 22:32:14 +1100

    Feature: sieve working :D

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-console.log("hey world (but typesafe heh)");
+import { primesUnder } from "./src/sieve";
+
+const max = 100;
+const primes = primesUnder(max);
+
+console.log(`there are ${primes.length} primes under ${max}:`);
+for(let i = 0; i < primes.length; i++) {
+    console.log(primes[i]);
+}
--- src/sieve.ts
+++ src/sieve.ts
@@ -1,9 +1,30 @@
-const generate_naturals = (max: number): number[] => {
+const generateNaturals = (max: number): number [] => {
 
-     const array = [];
+    const array = [];
     for(let i = 2; i < max; i++) {
         array.push(i);
     }
 
     return array;
+}
+
+const sieve = (naturals: number []): number [] => {
+    const max = naturals.length + 1;
+    let primes = naturals;
+
+    for(let base = 2; base < max; base++) {
+        primes = primes.filter(int => {
+            if (int % base === 0 && int !== base) {
+                return false;
+            } else {
+                return true;
+            }
+        });
+    }
+    
+    return primes;
+}
+
+export const primesUnder = (max: number): number [] => {
+    return sieve(generateNaturals(max));
 }
